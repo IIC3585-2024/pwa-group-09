39c5cb8c647f44a7270aa611ba939bad6a7d2bb4
sbvillalobosm <svillalobosm@uc.cl>
2024-05-13 10:29:22 -0400

    add detailed transaction view

--- public/js/edit_transaction.js
+++ public/js/edit_transaction.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById('edit-form');
+    const addDetailsButton = document.getElementById('add-details-button');
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -27,6 +28,55 @@ document.addEventListener('DOMContentLoaded', (event) => {
         };
 
         console.log(transactionForm);
+
+        saveTransactionForm(transactionForm);
+
+
         window.location.href = '/event';
     });
 })
+
+function saveTransactionForm(transactionForm) {
+    const request = window.indexedDB.open('transactionDB', 1);
+
+    request.onerror = function(event) {
+        console.error("Error al abrir la base de datos:", event.target.errorCode);
+    };
+
+    // Definir qué hacer cuando se abre la base de datos
+    request.onupgradeneeded = function(event) {
+        const db = event.target.result;
+
+        // Crear una tienda de objetos para almacenar el formulario
+        const objectStore = db.createObjectStore('transactions', { keyPath: 'id', autoIncrement: true });
+
+        // Definir los campos que se almacenarán
+        objectStore.createIndex('who_paid', 'who_paid', { unique: false });
+        objectStore.createIndex('for_what', 'for_what', { unique: false });
+        objectStore.createIndex('how_much', 'how_much', { unique: false });
+        objectStore.createIndex('when_date', 'when_date', { unique: false });
+        objectStore.createIndex('participants', 'participants', { unique: false });
+    };
+
+    // Definir qué hacer cuando se abre correctamente la base de datos
+    request.onsuccess = function(event) {
+        const db = event.target.result;
+
+        // Iniciar una transacción de lectura/escritura
+        const transaction = db.transaction(['transactions'], 'readwrite');
+
+        // Obtener el objeto de la tienda de objetos
+        const objectStore = transaction.objectStore('transactions');
+
+        // Agregar el formulario a la tienda de objetos
+        const addRequest = objectStore.add(transactionForm);
+
+        addRequest.onsuccess = function(event) {
+            console.log('Formulario guardado correctamente en IndexedDB.');
+        };
+
+        addRequest.onerror = function(event) {
+            console.error('Error al guardar el formulario en IndexedDB:', event.target.error);
+        };
+    };
+}
